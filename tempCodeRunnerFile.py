df68c5f4270960f40e3a5dd5a85bc4728145aba8
youssefahmed3 <78634572+youssefahmed3@users.noreply.github.com>
2023-12-23 01:37:50 +0200

    added figures for represenatation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-embed
+
+# Calculate the NC and PSNR values
+nc_ow_wi = calculate_nc(original_image, watermarked_image)
+psnr_ow_wi = calculate_psnr(original_image, watermarked_image)
+nc_wm_ew = calculate_nc(watermark_image, extracted_watermark)
